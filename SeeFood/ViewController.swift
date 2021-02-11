894972391cc487f7630f0214c242c53552d8ed16
darshilagrawal <darshilagrawal2000@gmail.com>
2021-02-11 22:35:30 +0530

    A Machine Learning Enabled iOS App
    An iOS App for detecting wether an image is of Hot Dog or Not Using Machine Learning

---
+++ SeeFood/ViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ViewController.swift
+//  SeeFood
+//
+//  Created by Darshil Agrawal on 06/10/20.
+//
+
+import UIKit
+import CoreML
+import Vision
+
+class ViewController: UIViewController,UINavigationControllerDelegate, UIImagePickerControllerDelegate{
+    @IBOutlet weak var imageView: UIImageView!
+    let imagePicker=UIImagePickerController()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.allowsEditing = false
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let pickedImage=info[UIImagePickerController.InfoKey.originalImage] as? UIImage{
+            imageView.image=pickedImage
+            guard let ciImage=CIImage(image: pickedImage)else{
+                fatalError("Error Converting Image to CIImage")
+            }
+            detect(image: ciImage)
+            }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    func detect(image:CIImage){
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else{
+            fatalError("Error Loading up MLModel")
+        }
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results=request.results as? [VNClassificationObservation] else{
+                fatalError("Error Fetching Classification results")
+            }
+            if let firstResult=results.first?.identifier{
+                if firstResult.contains("hotdog"){
+                    self.navigationItem.title="Hotdog!"
+                }else{
+                    self.navigationItem.title="Not Hotdog"
+                }
+            }
+            
+        }
+        let handler=VNImageRequestHandler(ciImage:image)
+        
+        do {
+            try handler.perform([request])
+        }catch{
+            print(error)
+        }
+    
+    }
+    
+    @IBAction func cameraButtonPressed(_ sender: UIBarButtonItem) {
+    
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+    
+}
+
